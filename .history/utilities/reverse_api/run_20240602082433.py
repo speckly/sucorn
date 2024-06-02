a8e54c88b853c903e73f3d916672916e76d639ef
djlptts <125202938+djlptts@users.noreply.github.com>
2024-06-02 08:26:24 -0400

    testing with "Return" key

--- .history/utilities/reverse_api/run_20240602082433.py
+++ .history/utilities/reverse_api/run_20240602082433.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         for account, token in cookies.items():
             open_console_window(account, token, prompt, out_path, args.delay, args.max)
 
-        keyboard.on_press_key('return', organize_windows)
+        keyboard.on_press_key('insert, organize_windows)
         keyboard.wait('end')
 
         terminate()
